4c61cffbc2c60e7b21ab1127d41da96dfa4cb64f
Rakesh Dodhia <94360865+r-dodhia@users.noreply.github.com>
2026-09-21 16:09:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Page setup
-st.set_page_config(page_title="Internal NHS consultancies", layout="wide")
+st.set_page_config(page_title="Internal NHS consultancies", page_icon="https://www.england.nhs.uk/wp-content/themes/nhsengland/favicon.ico", layout="wide")
 
 # Theme icon mapping 
 THEME_ICONS = {
